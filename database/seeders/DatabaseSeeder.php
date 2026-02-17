82bc26fed44dec5af75dc94f45d6d932fd3c12c9
Roel Delos Reyes <123723123+leorxdxd@users.noreply.github.com>
2026-02-17 18:02:27 +0800

    Update DatabaseSeeder to include Grade 7 exams

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             InitialUsersSeeder::class,
             IntelligenceExamSeeder::class,
+            Grade7ExamsSeeder::class,
             NormDataSeeder::class,
         ]);
     }
