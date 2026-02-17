e3b6fdcbb237c153aa3158f1ab039baf05a59590
Roel Delos Reyes <123723123+leorxdxd@users.noreply.github.com>
2026-02-17 18:06:25 +0800

    Fix Railway healthcheck: add /up route and increase timeout

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use App\Http\Controllers\Admin\BackupController;
 use App\Http\Controllers\Admin\ReportsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/up', function () {
+    return response()->json(['status' => 'ok']);
+});
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
